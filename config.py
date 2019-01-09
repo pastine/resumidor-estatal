64ea5d60bfcaa5b58d3348d4375eae5458a1de4a
Federico del Mazo <fdelmazo@fi.uba.ar>
2019-01-09 18:43:06 -0300

    Header and footer on reply coment

---
+++ config.py
@@ -0,0 +1,6 @@
+AUTHORS = "u/aaa u/bbb u/ccc u/ddd"
+REPLY_TO = "ResumidorEstatalDum"
+TIMEOUT = 60
+
+COMMENT_HEADER = """### Resumen de la noticia\n\n"""
+COMMENT_FOOTER = """\n\n---\n\nCreated by: """+AUTHORS+""" | [Source Code](https://github.com/pastine/resumidor-estatal)"""
--- resumidor_estatal.py
+++ resumidor_estatal.py
@@ -1,18 +1,29 @@
 import praw
 import time
 from summa import summarizer
-
+import config
 
 reddit = praw.Reddit('resumidor_estatal')
 
+def build_child_comment(parent_comment):
+    reply = config.HEADER
+    reply += summarize_news(parent_comment.body)
+    reply += config.FOOTER
+    return reply
+    
+def summarize_news(parent_comment_body):
+    summary = '>' # Summary must be quoted
+    summary += summarizer.summarize(parent_comment_body, language='spanish')
+    summary = summary.replace('\n','\n\n>') # Reddit uses markdown, where paragraphs are divided by two breaklines
+    return summary
 
 def watch_and_reply(comments_replied, file):
-    for comment in reddit.redditor('ResumidorEstatalDum').comments.new(limit=10):
+    for comment in reddit.redditor(config.REPLY_TO).comments.new(limit=10):
         if comment.id in comments_replied:
             print('Already replied to this comment :(')
             continue
         print('Found a comment! Time to reply.')
-        comment.reply(summarizer.summarize(comment.body, language='spanish'))
+        comment.reply(build_child_comment(comment))
         comments_replied.add(comment.id)
         file.write(comment.id+'\n')
         file.flush()
@@ -27,11 +38,11 @@ def main():
     while True:
         try:
             watch_and_reply(comments_replied, file)
-            print('Sleeping for 1 minute...')
-            time.sleep(60)
+            print(f'Sleeping for {config.TIMEOUT} seconds...')
+            time.sleep(config.TIMEOUT)
         except:
             print('Seems like we can\'t reply at the moment! Trying again in a minute')
-            time.sleep(60)
+            time.sleep(config.TIMEOUT)
     file.close()  #Heh
 
 
