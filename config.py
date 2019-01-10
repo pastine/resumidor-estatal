2b0b10db4f6c911978d7308b52070629e66a06b7
pastine <casimiropastine@gmail.com>
2019-01-10 20:38:11 -0300

    WIP - Validate if comments are replied by bot

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-AUTHORS = "u/aaa u/bbb u/ccc u/ddd"
+AUTHORS = "u/aaa u/bbb u/ddd"
 REPLY_TO = "ResumidorEstatalDum"
 TIMEOUT = 60
 
 COMMENT_HEADER = """### Resumen de la noticia\n\n"""
 COMMENT_FOOTER = """\n\n---\n\nCreated by: """+AUTHORS+""" | [Source Code](https://github.com/pastine/resumidor-estatal)"""
+
+ME = "ResumidorEstatalBot"
